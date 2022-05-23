53eeee3278334747d78d73d2ed095be1b654c039
rsuda <46731749+rsuda@users.noreply.github.com>
2022-05-23 15:31:29 -0500

    update

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface pastjobs {
+  title: string;
+  date: string;
+  id: string;
+  chip: string;
+}
+
+const PAST_JOBS_DATA: pastjobs[] = [
+  {title: "Stop 2 to Destination", date: "3/29", id: "T9637111HL", chip: "Submitted"},
+  {title: "Stop 1 to Stop 2", date: "3/29", id: "T9637111HL", chip: "Missing"},
+  {title: "Stop 2 to Destination", date: "3/28", id: "T9637111HL", chip: "Missing"},
+  {title: "Stop 1 to Stop 2", date: "3/27", id: "T9637111HL", chip: "Paid"},
+  {title: "Stop 3 to Destination", date: "3/27", id: "T9637111HL", chip: "Paid"},
+  {title: "Stop 2 to Stop 3", date: "3/27", id: "T9637111HL", chip: "Paid"}
+]
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +23,7 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  pastjobs = PAST_JOBS_DATA;
   constructor() {}
 
 }
